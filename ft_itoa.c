e74bdcd92b378093e1f59a028cb798ab69ddb4c1
Hilary Cloves <hcloves@il-h3.21-school.ru>
2019-09-24 18:17:26 +0300

    test

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,44 +6,61 @@
 /*   By: hcloves <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 14:03:55 by hcloves           #+#    #+#             */
-/*   Updated: 2019/09/24 14:55:33 by hcloves          ###   ########.fr       */
+/*   Updated: 2019/09/24 18:16:18 by hcloves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static int        ft_clen(int n)
 {
-	char sign;
-	size_t i;
-	size_t count;
+	int len;
+
+	len = 0;
+	while (n /= 10)
+		len++;
+	return (len + 1);
+}
+
+char    *ft_itoa(int n)
+{
+	int sign;
+	int i;
+	int count;
 	char *numstr;
-	size_t temp;
+	char *numstr2;
 
 	i = 0;
-	count - 0;
-	temp = n;
-	if (n < 0)
-	{
-		sign = '-';
+	sign = (n < 0) ? 1 : 0;
+	if (sign)
 		n *= -1;
-	}
-	if (n == 0)
-		return ('0');
-	while (n % 10)
+	count = ft_clen(n);
+	if ((numstr = (char *)malloc(sizeof(char *) * (count + 1))))
+		return (NULL);
+	while (i < count)
 	{
-		count++;
+		numstr[i++] = (n % 10) + '0';
 		n = n / 10;
 	}
-	if ((numstr = (char)malloc(sizeof(char *) * count + 1)))
-	{
-		while (i < count)
-		{
-			numstr[i] = (n % 10) + '0';
-			n = n / 10;
-			i++;
-		}
-	}
-	count[i] = '\0';
-	return (numstr);
+	if (!(numstr2 = (char *)malloc(sizeof(char *) * (count + 2))))
+		return (NULL);
+	count = 0;
+	if (sign)
+		numstr2[count++] = '-';
+		//count++;
+	i--;
+	while (i >= 0)
+		numstr2[count++] = numstr[i--];
+		//i--;
+		//count++;
+	numstr2[count] = '\0';
+	return (numstr2);
+}
+
+int main()
+{
+	size_t n;
+	n = -103120;
+	printf("%s", ft_itoa(n));
+	return 0;
 }
